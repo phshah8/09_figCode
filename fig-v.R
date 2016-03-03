994a1074b8c9bf653a516d94ae1beb86fd3f1796
Pankti <phshah8@asu.edu>
2016-03-02 23:28:01 -0700

    update libraries and makefile

--- fig-v.R
+++ fig-v.R
@@ -1,4 +1,26 @@
 #5 Exercise
+libs <- c('ggplot2', 'latticeExtra', 'gridExtra', 'MASS', 
+          'colorspace', 'plyr', 'Hmisc', 'scales')
+lapply(libs, require, character.only = T)
+
+data(diamonds)
+#1 Exercise
+diamonds.ideal<- subset(diamonds, diamonds$cut=="Ideal")
+diamonds.rest.nonJ<-subset(diamonds, diamonds$cut != "Ideal" & diamonds$color != "J")
+
+#2 Exercise
+diamonds.ideal.carat.mean<- aggregate(diamonds.ideal$carat, by = list(diamonds.ideal$clarity), FUN=mean)
+diamonds.ideal.carat.median<- aggregate(diamonds.ideal$carat, by = list(diamonds.ideal$clarity), FUN=median)
+
+#3 Exercise
+names(diamonds.ideal.carat.mean) <- c("Clarity", "MeanIncarat")
+diamonds.order<- diamonds.ideal.carat.mean[order(diamonds.ideal.carat.mean$MeanIncarat),]
+
+#4 Exercise
+names(diamonds.ideal.carat.median)<-c("Clarity", "MedianIncarat")
+diamonds.mean.median<-merge(diamonds.ideal.carat.mean, diamonds.ideal.carat.median, by="Clarity")
+
+
 setwd("./")
 pdf("fig-v.pdf")
 xyplot(diamonds.mean.median$MeanIncarat ~ diamonds.mean.median$MedianIncarat, data=diamonds.mean.median,
--- fig-vi.R
+++ fig-vi.R
@@ -1,3 +1,14 @@
+libs <- c('ggplot2', 'latticeExtra', 'gridExtra', 'MASS', 
+          'colorspace', 'plyr', 'Hmisc', 'scales')
+lapply(libs, require, character.only = T)
+
+data(diamonds)
+
+#1 Exercise
+diamonds.ideal<- subset(diamonds, diamonds$cut=="Ideal")
+diamonds.rest.nonJ<-subset(diamonds, diamonds$cut != "Ideal" & diamonds$color != "J")
+
+
 #6 Exercise
 setwd("./")
 pdf("fig-vi.pdf")
